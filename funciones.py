2c1df84f95e289f4ecf8d10b514a9d18a4de314b
209 <209@academicos.cesde.edu.co>
2022-10-01 11:07:19 -0500

    Modificaciones

--- funciones.py
+++ funciones.py
@@ -1,8 +1,13 @@
 #funcion tradicional para sumar dos numeros
 
-def sumar(numero1, numero2)
+def sumar(numero1, numero2):
     return numero1+numero2
 
 #invocando la funcion total
 resultado= sumar(5,10)
-print("la suma es: {resultado}")
+print(f"la suma es: {resultado}")
+
+if resultado > 10:
+    print("Ganaste")
+else:
+    print("Perdio")
