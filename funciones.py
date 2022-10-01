e04bc4c2ed5819931c7c689a75bf1d845e086e01
209 <209@academicos.cesde.edu.co>
2022-10-01 11:01:17 -0500

    Agrega funciones

---
+++ funciones.py
@@ -0,0 +1,8 @@
+#funcion tradicional para sumar dos numeros
+
+def sumar(numero1, numero2)
+    return numero1+numero2
+
+#invocando la funcion total
+resultado= sumar(5,10)
+print("la suma es: {resultado}")
